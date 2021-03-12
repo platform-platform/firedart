1702077b76bdf303537a2c9827ee10c1a63f143b
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-03-12 12:25:13 +0200

    feat: Export FirebaseAuthException (#7)
    * Export FirebaseAuthException
    
    * Update error codes
    
    * Documentation and readability improvements

--- lib/src/auth/mapper/firebase_auth_error_code_mapper.dart
+++ lib/src/auth/mapper/firebase_auth_error_code_mapper.dart
@@ -6,24 +6,25 @@ class FirebaseAuthErrorCodeMapper {
   /// Maps the given [code] into the human-readable error description.
   static String map(String code) {
     switch (code) {
-      case FirebaseAuthErrorCode.invalidEmail:
-        return 'The email address is badly formatted.';
-      case FirebaseAuthErrorCode.wrongPassword:
+      case FirebaseAuthErrorCode.emailNotFound:
+        return 'There is no user record corresponding to this identifier. The user may have been deleted.';
+      case FirebaseAuthErrorCode.invalidPassword:
         return 'The password is invalid or the user does not have a password.';
       case FirebaseAuthErrorCode.userNotFound:
         return 'There is no user record corresponding to this identifier. '
             'The user may have been deleted.';
       case FirebaseAuthErrorCode.userDisabled:
         return 'The user account has been disabled by an administrator.';
-      case FirebaseAuthErrorCode.tooManyRequests:
+      case FirebaseAuthErrorCode.tooManyAttemptsTryLater:
         return 'We have blocked all requests from this device due to unusual '
             'activity. Try again later.';
       case FirebaseAuthErrorCode.operationNotAllowed:
         return 'This operation is not allowed. Enable the sign-in method in '
             'the Authentication tab of the Firebase console.';
-      default:
-        return 'An unknown error occurred with the following code: $code. '
-            'Please check your Internet connection and try again later.';
+      case FirebaseAuthErrorCode.invalidApiKey:
+        return 'The Firebase API key is invalid.';
     }
+    return 'An unknown error occurred with the following code: $code. '
+        'Please check your Internet connection and try again later.';
   }
 }
